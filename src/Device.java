b44601ebfc6e0ec93013bbec51efb2a230f1b82f
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2017-12-02 16:47:46 +0100

    zadanie-java-33-poprawione

--- src/Device.java
+++ src/Device.java
@@ -1,22 +1,26 @@
 public class Device {
 
-        String code;
-        String producer;
-        String type;
-        double price;
+    String code;
+    String producer;
+    String type;
+    double price;
 
-            Device(String c, String pr, String t, double p){
-            code = c;
-            producer = pr;
-            type = t;
-            price = p;
+            Device(String code, String producer, String type, double price) {
+                this(code, producer, type);
+                this.price = price;
+            }
+
+            Device(String code, String producer, String type) {
+                this.code = code;
+                this.producer = producer;
+                this.type = type;
             }
 
             Device() {
             }
 
-        void getInfo () {
-        System.out.println("Kod: " + code + ", producent: " + producer + ", typ: " + type + ", cena: " + price);
-        }
+    String getInfo() {
+        return "Kod: " + code + ", producent: " + producer + ", typ: " + type + ", cena: " + price;
+    }
 
 }
--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,13 +1,13 @@
 public class Warehouse {
      public static void main(String[] args) {
 
-                Device device1 = new Device("QE65Q7FAM", "Samsung", "TV", 9499.99); //code, producer, type, price
-                Device device2 = new Device("P9 Lite", "Huawei", "Smatyphone", 0); //code, producer, type
-                Device device3 = new Device("Playstation 4", "Sony", "Smatyphone", 1899.99); //code, producer, type, price
+                Device device1 = new Device("QE65Q7FAM", "Samsung", "TV", 9499.99);
+                Device device2 = new Device("P9 Lite", "Huawei", "Smatyphone", 0);
+                Device device3 = new Device("Playstation 4", "Sony", "Smatyphone", 1899.99);
 
-                device1.getInfo();
-                device2.getInfo();
-                device3.getInfo();
+                device2.price = 0;
 
+                String allDevices = device1.getInfo() + "\n" + device2.getInfo() + "\n" + device3.getInfo();
+                System.out.println(allDevices);
             }
- }
+}
