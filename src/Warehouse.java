4f67ce2cd39a2ec4ff65da3a784c92514f3a3d04
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2017-12-30 17:03:32 +0100

    zadanie-java-33-ulepszone (klasa Warehouse teraz jest taka sama jak w zadaniu)

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -2,10 +2,8 @@ public class Warehouse {
      public static void main(String[] args) {
 
                 Device device1 = new Device("QE65Q7FAM", "Samsung", "TV", 9499.99);
-                Device device2 = new Device("P9 Lite", "Huawei", "Smatyphone", 0);
+                Device device2 = new Device("P9 Lite", "Huawei", "Smatyphone");
                 Device device3 = new Device("Playstation 4", "Sony", "Smatyphone", 1899.99);
-
-                device2.price = 0;
 
                 String allDevices = device1.getInfo() + "\n" + device2.getInfo() + "\n" + device3.getInfo();
                 System.out.println(allDevices);
